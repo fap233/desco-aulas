fdd135dbb9906ea43dc2b091750ba8e2dfc05fff
fo0lest <fellipe306@gmail.com>
2024-09-18 15:04:59 -0300

    commit inicial

---
+++ node_6f8a3d9a33219.tmp.js
@@ -0,0 +1,14 @@
+let i = 1
+let text = ""
+while (i < 20) {
+    text = "the number is " + i;
+    console.log(text);
+    
+   
+    while (i <= 10) {
+        text = "bug";
+        console.log(text);
+        i++;
+        
+    }
+}
---
+++ while.js
@@ -0,0 +1,7 @@
+let i = 0
+let text = ""
+while (i <= 10) {
+    text = "the number is " + i;
+    i++;
+    console.log(text)
+}
